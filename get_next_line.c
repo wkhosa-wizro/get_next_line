52a27379d3cf45fb305e4bf4aa683d0c7a87734a
Wisani KHOSA <wkhosa@e5r9p11.wethinkcode.co.za>
2017-09-18 18:15:14 +0200

    new approach to get_next_line, still need to be fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,75 +6,95 @@
 /*   By: wkhosa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/13 13:07:23 by wkhosa            #+#    #+#             */
-/*   Updated: 2017/09/13 18:01:29 by wkhosa           ###   ########.fr       */
+/*   Updated: 2017/09/18 18:05:59 by wkhosa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t		ft_strlen_to_c(char const *s, char const c)
+/*
+int		get_next_line(int const fd, char **line)
 {
-	size_t	len;
+//	return (-1);
 
-	len = 0;
-	if (ft_strchr(s, c) == NULL)
-		return (ft_strlen(s));
-	while (*s != c)
-	{
-		s++;
-		len++;
-	}
-	return (len);
-}
+	if (buff is not initialised)
+		initialise buff
 
-void	reset(char **s)
-{
-	char	*temp;
-	size_t	len;
+	initialise temp
 
-	temp = ft_strchr(*s, '\n');
-	if (temp == NULL)
-		return ;
-	else
+	nbyte = read(fd, temp, BUFF_SIZE);
 	{
-		temp++;
-		len = ft_strlen(temp);
-		ft_memmove(*s, temp, len);
-	}
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	static	char	*temp = NULL;
-	char			*buffer;
-	int				byts_read;
-	
-	if (!(buffer = ft_strnew(BUFF_SIZE)))
-		return (-1);
-	byts_read = read(fd, buffer, BUFF_SIZE);
-	buffer[byts_read] = '\0';
-	if (temp == NULL)
-	{
-		if (!(temp = ft_strnew(2 * BUFF_SIZE)))
-			return (-1);
-		temp[0] = '\0';
-	}
-	ft_strcat(temp,buffer);
-	ft_strcat(*line, temp);
-	while (!(ft_strchr(temp, '\n')))
-	{
-//		ft_putstr(">>>> line ");
-//		ft_putendl(*line);
-		byts_read = read(fd, buffer, BUFF_SIZE);	
-		buffer[byts_read] = '\0';
-		ft_strcpy(temp, buffer);
-		ft_strncat(*line, temp, ft_strlen_to_c(temp, '\n'));
-		if (byts_read == 0)
+		ft_strcat(buff, temp);
+		nbyte < BUFF_SIZE;
 		{
-			reset(&temp);
-			return (0);
+			take data from buff up to endl or newl to line
+				if (newl comes before endl)
+					shift data in buff
+					return (1)
+				else
+					free buff
+			return (0)
+		}
+		nbyte == BUFF_SIZE;
+		{
+			take data from buff up to newl or endl to line
+				if (newl comes before endl)
+					shift data in buff
+					return (1);
+				else
+					free buff
+					gnl
+				return (1)
+		}
+		nbyte == 0;
+		{
+			take data from buff to line
+			free buff
+			return (0)
 		}
 	}
-	reset(&temp);
-	return (1);
+	return (0)
+}
+*/
+
+#include <stdio.h>
+
+int		get_next_line(int const fd, char **line)
+{
+	static	char	*buff = NULL;
+	char			*temp;
+	int				nbyte;
+
+	if (!(buff))
+	{
+		if (!(buff = ft_strnew(2 * BUFF_SIZE))) 
+			return (-1);
+	//	printf(">>> buff initialising buff = %s\n", buff);
+	}
+	if (!(temp = ft_strnew(BUFF_SIZE)) || fd < 0)
+		return (-1);
+	ft_strclr(*line);
+	while ((nbyte = read(fd, temp, BUFF_SIZE)) > 0)
+	{
+		temp[nbyte] = '\0';
+	//	printf("#1>>> nbyte = %d, temp = %s '\n", nbyte, temp);
+		if (ft_strchr(ft_strcat(buff , temp), '\n'))
+		{
+	//		printf("#2>>> buff = %s, temp = %s '\n", buff, temp);
+			ft_strdel(&temp);
+		//	ft_strclr(*line);
+			temp = ft_strsub(buff, 0, ft_strchr(buff, '\n') - buff + 1);
+	//		printf("#3>>> buff = %s, temp = %s '\n", buff, temp);
+			ft_memmove(buff, ft_strchr(buff, '\n') + 1, ft_strlen(ft_strchr(buff, '\n')) + 1);
+	//		printf("#4>>> line = %s  buff = %s, temp = %s '\n", *line, buff, temp);
+			ft_strncat(*line, temp, ft_strchr(temp, '\n') - temp);
+	//		printf("#5>>> line = %s  buff = %s, temp = %s '\n", *line, buff, temp);
+			return (1);
+		}
+	//		printf("#6>>> line = %s  buff = %s, temp = %s '\n", *line, buff, temp);
+		ft_strcat(*line, buff);
+		ft_strclr(buff);
+	//		printf("#7>>> line = %s  buff = %s, temp = %s '\n", *line, buff, temp);
+	}
+	//		printf("#8>>> line = %s  buff = %s, temp = %s '\n", *line, buff, temp);
+	return (0);
 }
